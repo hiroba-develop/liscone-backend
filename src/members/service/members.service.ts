fb34f44387753e7398a94eb076d9cca585f658d5
JushinSeo <jushin@cydas.com>
2023-06-27 09:17:37 +0900

    로그인

--- src/members/service/members.service.ts
+++ src/members/service/members.service.ts
@@ -1,4 +1,9 @@
-import { Inject, Injectable, NotFoundException, UnprocessableEntityException } from '@nestjs/common';
+import {
+  Inject,
+  Injectable,
+  NotFoundException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateMemberDTO } from '../dto/create-member.dto';
@@ -7,58 +12,74 @@ import { UpdateMemberDTO } from '../dto/update-member.dto';
 
 @Injectable()
 export class MembersService {
-    constructor(
-        @InjectRepository(MemberEntity)
-        private membersRepository: Repository<MemberEntity>,
-    ) { }
+  constructor(
+    @InjectRepository(MemberEntity)
+    private membersRepository: Repository<MemberEntity>,
+  ) {}
 
-    findAll(): Promise<MemberEntity[]> {
-        return this.membersRepository.find();
-    }
+  findAll(): Promise<MemberEntity[]> {
+    return this.membersRepository.find();
+  }
 
-    findByMemberId(member_id: string): Promise<MemberEntity> {
-        return this.membersRepository.findOne({
-            where: {
-                member_id,
-            }
-        });
-    }
-    
-    findByMemberName(member_name: string): Promise<MemberEntity> {
-        return this.membersRepository.findOne({
-            where: {
-                member_name,
-            }
-        });
-    }
-    
-    async create(member: CreateMemberDTO) {
-        await this.membersRepository.save(member)
-    }
+  findByMemberId(member_id: string): Promise<MemberEntity> {
+    return this.membersRepository.findOne({
+      where: {
+        member_id,
+      },
+    });
+  }
 
-    async update(member: UpdateMemberDTO) {
-        const resultMember = await this.findByMemberId(member.member_id);
-        if (!resultMember) {
-            throw new NotFoundException("member id is not exist");
-        }
-        try{
-            await this.membersRepository.update(                
-                    member.member_id ,
-                {                 
-                    company_code: member.company_code,
-                    member_name: member.member_name,
-                    password: member.password,
-                    dormant_status: member.dormant_status,
-                    password_expired_day: member.password_expired_day,    
-                });
-            return await this.findByMemberId(member.member_id);
-        } catch (e) {
-            console.log('there is no member having id : ' + member.member_id);
-            throw e;
-        }
+  findByAuth(member_id: string, password: string): Promise<MemberEntity> {
+    return this.membersRepository.findOne({
+      where: {
+        member_id,
+        password,
+      },
+    });
+  }
+
+  findByMemberName(member_name: string): Promise<MemberEntity> {
+    return this.membersRepository.findOne({
+      where: {
+        member_name,
+      },
+    });
+  }
+
+  async login(member_id, password: string): Promise<MemberEntity> {
+    console.log(member_id, password);
+    const resultMember = await this.findByAuth(member_id, password);
+    if (null == resultMember) {
+      throw new NotFoundException('member id is not exist');
     }
-    
-    async remove(id: string): Promise<void> {
-        await this.membersRepository.delete(id);
+    return resultMember;
+  }
+
+  async create(member: CreateMemberDTO) {
+    await this.membersRepository.save(member);
+  }
+
+  async update(member: UpdateMemberDTO) {
+    const resultMember = await this.findByMemberId(member.member_id);
+    if (!resultMember) {
+      throw new NotFoundException('member id is not exist');
     }
+    try {
+      await this.membersRepository.update(member.member_id, {
+        company_code: member.company_code,
+        member_name: member.member_name,
+        password: member.password,
+        dormant_status: member.dormant_status,
+        password_expired_day: member.password_expired_day,
+      });
+      return await this.findByMemberId(member.member_id);
+    } catch (e) {
+      console.log('there is no member having id : ' + member.member_id);
+      throw e;
+    }
+  }
+
+  async remove(id: string): Promise<void> {
+    await this.membersRepository.delete(id);
+  }
 }
