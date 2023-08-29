7f16ad73419a2c968c93cd93ccd0fe07b1f28c37
yuta-toshino <toshino12311@gmail.com>
2023-08-30 02:55:28 +0900

    リスト作成ボタンエラー対応

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { urlencoded, json } from 'express';
 
 async function bootstrap() {
   const port = process.env.SERVER_PORT;
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.use(json({ limit: '100mb' }));
+  app.use(urlencoded({ extended: true, limit: '100mb' }));
   await app.listen(port);
 }
 bootstrap();
